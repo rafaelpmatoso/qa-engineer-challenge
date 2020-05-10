2d028de229bc95e2645423cdde118291b7ec9b08
Rafael Matoso <rmatoso@orizon.com.br>
2020-05-10 15:49:37 -0300

    project build

--- src/test/java/runner/RunCukesTest.java
+++ src/test/java/runner/RunCukesTest.java
@@ -8,9 +8,9 @@ import io.cucumber.junit.CucumberOptions;
 import webdriver.DriverFactory;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(strict = true, features = { "src/test/resources/features" }, glue = { "steps" }, tags = {
-		"@BuscaCursos" })
-public class Runner {
+@CucumberOptions(strict = true, monochrome = true, features = { "src/test/resources/features" }, glue = {
+		"steps" }, tags = { "@BuscaCursos" })
+public class RunCukesTest {
 
 	@AfterClass
 	public static void closeDriver() {
